021d0cdf3745732d7ca3a8a226ad90984f8a6ea8
roshan <roshan@feyzilogic.com>
2021-03-19 09:43:35 -0400

    comment noncompliant iam

--- staging/main.tf
+++ staging/main.tf
@@ -3,24 +3,24 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_iam_policy" "basically_allow_all" {
-  name        = "some_policy"
-  path        = "/"
-  description = "Some policy"
+#resource "aws_iam_policy" "basically_allow_all" {
+#  name        = "some_policy"
+#  path        = "/"
+#  description = "Some policy"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "*",
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
+#  policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "*",
+#      "Effect": "Allow",
+#      "Resource": "*"
+#    }
+#  ]
+#}
+#EOF
+#}
 
 resource "aws_iam_policy" "basically_deny_all" {
   name        = "some_policy"
