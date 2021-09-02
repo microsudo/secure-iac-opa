93fece463f68fe6b558c10ba5252b34c7375ddbf
roshan <roshan@feyzilogic.com>
2021-09-02 10:34:07 -0400

    andd bad IAM policy

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
 provider "aws" {
-  version = "~> 2.41"
-  region  = "us-west-2"
+  region = "us-west-1"
+}
 
+resource "aws_vpc" "my_fugue_cicd_vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "my-fugue-cicd-vpc"
+  }
+}
 
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.my_fugue_cicd_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_iam_policy" "basically_deny_all" {
   name        = "some_policy"
   path        = "/"
@@ -22,4 +39,3 @@ resource "aws_iam_policy" "basically_deny_all" {
   ]
 }
 EOF
-}
